a23eec1732c7d88e67c48ba4c15c8d23db5977c6
HanetakaChou <HanetakaChou@outlook.com>
2025-01-15 21:29:00 +0100

    compressed fonts

---
+++ misc/fonts/noto_sans_simplified_chinese_ttf_compressed.h
@@ -0,0 +1,8 @@
+#ifndef _NOTO_SANS_SIMPLIFIED_CHINESE_TTF_COMPRESSED_H_
+#define _NOTO_SANS_SIMPLIFIED_CHINESE_TTF_COMPRESSED_H_ 1
+
+static constexpr unsigned int const noto_sans_simplified_chinese_ttf_compressed_size = 13807374;
+
+extern unsigned char const *const noto_sans_simplified_chinese_ttf_compressed_data_base;
+
+#endif
---
+++ misc/fonts/noto_sans_traditional_chinese_ttf_compressed.h
@@ -0,0 +1,8 @@
+#ifndef _NOTO_SANS_TRADITIONAL_CHINESE_TTF_COMPRESSED_H_
+#define _NOTO_SANS_TRADITIONAL_CHINESE_TTF_COMPRESSED_H_ 1
+
+static constexpr unsigned int const noto_sans_traditional_chinese_ttf_compressed_size = 9288174;
+
+extern unsigned char const* const noto_sans_traditional_chinese_ttf_compressed_data_base;
+
+#endif
